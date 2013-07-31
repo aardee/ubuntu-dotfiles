24f083ba0d4ae07adcacf9129788b51c890747c5
pjan vandaele <pjan.vandaele@gmail.com>
2013-07-31 16:47:46 +0800

    Bugfix in bootstrap script.

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,7 +5,7 @@ function doIt() {
     rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
         --exclude "README.md" --exclude "LICENSE" \
         -av --no-perms . ~
-    source ~/.bash_profile
+    source ~/.profile
 }
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
     doIt
