0916b5272e4649276c7117e1ba5dbc064ef89689
pjan vandaele <pjan@pjan.io>
2014-08-27 17:02:31 +0900

    Updated bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,19 +1,40 @@
 #!/usr/bin/env bash
-cd "$(dirname "${BASH_SOURCE}")"
-git pull origin master
-function doIt() {
-    rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
-        --exclude "README.md" --exclude "LICENSE" \
-        -av --no-perms . ~
-    source ~/.profile
+
+# Functions
+update_repo() {
+  cd "$(dirname "${BASH_SOURCE}")"
+  git pull origin master
 }
+
+install_repo() {
+    cd "$(dirname "${BASH_SOURCE}")"
+  rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
+    --exclude "README.md" --exclude "LICENSE" \
+    -av --no-perms . ~
+}
+
+resource() {
+  source ~/.profile
+}
+
+doIt() {
+  update_repo
+  install_repo
+  resource
+}
+
+# MAIN
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
     doIt
 else
-    read -p "Updating vim-config may overwrite existing customized configuration. Are you sure to proceed? (y/n) " -n 1
+    read -p "Updating ubuntu-dotfiles may overwrite existing customized configuration. Are you sure to proceed? (y/n) " -n 1
     echo
     if [[ $REPLY =~ ^[Yy]$ ]]; then
         doIt
     fi
 fi
+
+unset update_repo
+unset install_repo
+unset resource
 unset doIt
